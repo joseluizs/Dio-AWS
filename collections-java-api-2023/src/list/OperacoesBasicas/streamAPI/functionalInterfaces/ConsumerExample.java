f6339d188a3ea65db765a9f2c8c1a185a40d3aa5
joseluizs <joseluiz.smarques@gmail.com>
2023-08-28 21:14:53 -0300

    Criando Consumer

---
+++ collections-java-api-2023/src/list/OperacoesBasicas/streamAPI/functionalInterfaces/ConsumerExample.java
@@ -0,0 +1,25 @@
+package streamAPI.functionalInterfaces;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+	public static void main(String[] args) {
+		List<Integer> numeros = Arrays.asList(1,2,3,4,56,7,8);
+		
+		Consumer<Integer> imprimirNumeroPar = numero ->{
+			if (numero % 2 == 0) {
+				System.out.println(numero);
+			}
+		};
+		numeros.stream().forEach(imprimirNumeroPar);
+		
+		numeros.forEach(n ->{
+			if (n % 2 == 0) {
+				System.out.print(n + " ");
+			}
+		});
+	}
+
+}
