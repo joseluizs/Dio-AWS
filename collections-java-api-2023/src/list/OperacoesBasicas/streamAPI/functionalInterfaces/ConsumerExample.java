6d84c4d0ed06e4aaf17b0ef530037cec6515e484
joseluizs <joseluiz.smarques@gmail.com>
2023-08-29 08:54:14 -0300

    Update ConsumerExample.java

--- collections-java-api-2023/src/list/OperacoesBasicas/streamAPI/functionalInterfaces/ConsumerExample.java
+++ collections-java-api-2023/src/list/OperacoesBasicas/streamAPI/functionalInterfaces/ConsumerExample.java
@@ -5,21 +5,29 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerExample {
-	public static void main(String[] args) {
-		List<Integer> numeros = Arrays.asList(1,2,3,4,5,6,7,8);
-		
-		Consumer<Integer> imprimirNumeroPar = numero ->{
-			if (numero % 2 == 0) {
-				System.out.println(numero);
-			}
-		};
-		numeros.stream().forEach(imprimirNumeroPar);
-		
-		numeros.forEach(n ->{
-			if (n % 2 == 0) {
-				System.out.print(n + " ");
-			}
-		});
-	}
+    public static void main(String[] args) {
+        // Criando uma lista de números
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
 
+        // Criando um Consumer para imprimir números pares
+        Consumer<Integer> imprimirNumeroPar = numero -> {
+            if (numero % 2 == 0) { // Verificando se o número é par
+                System.out.println(numero); // Imprimindo o número par
+            }
+        };
+
+        // Usando a Stream API para aplicar o Consumer em cada número da lista
+        numeros.stream().forEach(imprimirNumeroPar);
+
+        // Usando forEach diretamente na lista para imprimir números pares
+        numeros.forEach(n -> {
+            if (n % 2 == 0) { // Verificando se o número é par
+                System.out.print(n + " "); // Imprimindo o número par seguido de espaço
+            }
+        });
+
+        // Usando a Stream API para filtrar e imprimir números pares
+        numeros.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
+    }
 }
+
