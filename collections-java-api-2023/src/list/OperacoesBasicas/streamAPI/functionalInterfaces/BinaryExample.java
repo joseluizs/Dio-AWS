b93e8455fda309b699d6cae8883d46c3f32e765a
joseluizs <joseluiz.smarques@gmail.com>
2023-08-29 10:38:05 -0300

    Update BinaryExample.java

--- collections-java-api-2023/src/list/OperacoesBasicas/streamAPI/functionalInterfaces/BinaryExample.java
+++ collections-java-api-2023/src/list/OperacoesBasicas/streamAPI/functionalInterfaces/BinaryExample.java
@@ -9,15 +9,24 @@ public class BinaryExample {
         // Criando uma lista de números
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
         
-        // Criando um BinaryOperator para somar dois números
+        // Criando um BinaryOperator para somar dois números (usando expressão lambda)
         BinaryOperator<Integer> somar = (num1, num2) -> num1 + num2;
+        
+        // Criando um BinaryOperator para somar dois números (usando referência de método)
+        BinaryOperator<Integer> somar2 = Integer::sum;
         
         // Usando a Stream API para reduzir (ou acumular) os números através da soma
         // O valor inicial para acumulação é 0
-        int resultado = numeros.stream().reduce(0, somar);
+        int resultado = numeros.stream().reduce(0, somar2);
         
         // Imprimindo o resultado da soma dos números
         System.out.println("A soma dos números é: " + resultado);
+        
+        // Usando o método sum da classe Integer para realizar a soma
+        int novoResultado = numeros.stream().reduce(0, Integer::sum);
+        
+        // Imprimindo o novo resultado da soma dos números
+        System.out.println("A soma do novo resultado dos números é: " + novoResultado);
     }
 }
 
